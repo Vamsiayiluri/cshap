656bf79822bd2e2351f1889af1c2bfd7a392a01a
VamsiAyiluri <vamsi.ayiluri@cognine.com>
2023-05-23 16:44:02 +0530

    csharp 1st commit

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "21121221111111111111";
+            if (input.Length < 4)
+            {
+                Console.WriteLine("invalid input");
+            }
+            else
+            {
+                Program product = new Program();
+                int result = product.getMaxProduct(input);
+                Console.WriteLine(result);
+                
+            }
+        }
+
+        public int getMaxProduct(string input)
+        {
+            int maxProduct = 0;
+            for (int i = 0; i < input.Length - 3; i++)
+            {
+                int tempProduct = 1;
+                for (int j = 0; j < 4; j++)
+                {                   
+                    char k = input[i + j];
+                    int val = (int)Char.GetNumericValue(k);                   
+                    tempProduct = tempProduct * val;
+                }
+                if (tempProduct > maxProduct)
+                {                    
+                    maxProduct = tempProduct;                    
+                }               
+            }
+            return maxProduct;
+        }
+    }
+}
---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string S1 = "abcdabcabd";
+            string S2 = "ab";
+            List<int> indexes = new List<int>();
+
+            int count = 0;
+            for (int i = 0; i <= S1.Length - S2.Length; i++)
+            {
+                if (S1.Substring(i, S2.Length) == S2)
+                {
+                    count++;
+                    indexes.Add(i);
+
+                }
+            }
+            Console.WriteLine($"Number of times occurred: {count}");
+            Console.WriteLine($"Index Positions: ");
+            for (int i = 0; i < indexes.Count; i++)
+            {
+                Console.Write(indexes[i] + " ");
+            }
+
+
+        }
+    }
+}
